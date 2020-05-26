5a7d3738a4a4a264f1e007f6beb9e22e0550680e
devchea <rchea817@gmail.com>
2020-05-26 09:43:04 -0700

    update

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,11 @@ class User < ApplicationRecord
   has_many :comments, through: :posts
   has_many :likes, through: :posts
   has_many :dislike, through: :posts
+
+
+  def get_content
+    self.posts.each do |post|
+      post.content
+    end
+  end
 end

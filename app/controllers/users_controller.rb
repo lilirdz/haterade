3529e01dcd721352ed3428690229547cd0ad386a
devchea <rchea817@gmail.com>
2020-05-26 14:14:20 -0700

    update

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,6 @@
 class UsersController < ApplicationController
 
-  before_action :current_user, only: [:show, :edit]
+  before_action :current_user, only: [:show, :edit, :update]
 
   def index
     @users = User.all
@@ -15,5 +15,21 @@ class UsersController < ApplicationController
 
   def edit
   end
+
+  def update
+    byebug
+      if @user.update(user_params)
+        flash[:success] = "Object was successfully updated"
+        redirect_to @user
+      else
+        flash[:error] = "Something went wrong"
+        redirect_to edit_user_path(@user)
+      end
+  end
+
+  private
+    def user_params
+      params.require(:user).permit(:name, :password, :username, :bio)
+    end
 
 end

4d74062208ae6640529aa6fe606b797cc9481e30
devchea <rchea817@gmail.com>
2020-05-27 08:15:18 -0700

    changes to form for posts

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,16 +8,16 @@ class User < ApplicationRecord
   extend FriendlyId
   friendly_id :username, use: :slugged
 
-  def total_likes 
-    sum = 0
+  # def total_likes 
+  #   sum = 0
 
-    self.posts.each do |post|
-      if post.likes
-        sum += post.likes
-      else
-        post.likes = 0
-      end
-    end    
-    sum
-  end
+  #   self.posts.each do |post|
+  #     if post.likes
+  #       sum += post.likes
+  #     else
+  #       post.likes = 0
+  #     end
+  #   end    
+  #   sum
+  # end
 end

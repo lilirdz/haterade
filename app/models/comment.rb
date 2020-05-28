222b94549fb3d09f7fc268da6160ea28f4a92620
lilirdz <lilimrod@gmail.com>
2020-05-27 22:39:31 -0500

    added validations

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,4 +1,6 @@
 class Comment < ApplicationRecord
   belongs_to :post
   belongs_to :user
+
+  validates :content, length: {minimum:2, maximum:140}
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -4,6 +4,10 @@ class Post < ApplicationRecord
   has_many :comments
   has_many :likes
   has_many :dislikes
+
+  validates :title, presence: true
+  validates :content, length: {minimum:2, maximum:140}
+  
   extend FriendlyId
   friendly_id :title, use: :slugged
 

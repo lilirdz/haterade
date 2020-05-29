babd7b23d950ef827d692453ceeb097f4d1020d0
lilirdz <lilimrod@gmail.com>
2020-05-29 12:08:13 -0500

    added to home page

--- app/models/category.rb
+++ app/models/category.rb
@@ -3,4 +3,8 @@ class Category < ApplicationRecord
   has_many :users, through: :posts
   extend FriendlyId
   friendly_id :name, use: :slugged
+
+  def self.category_most_posts
+    self.all.sort {|a,b| b.posts.count <=> a.posts.count}.first
+  end
 end
